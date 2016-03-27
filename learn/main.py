aa46c32127adc8f7ecf7019935516430f3dee55a
Joel Roggeman <joelroggeman@gmail.com>
2016-03-27 15:44:09 -0400

    Refactoring

--- learn/main.py
+++ learn/main.py
@@ -4,70 +4,13 @@ import sys
 
 
 def engage(initialize_models, train_with_example, model_test, filename='../data/reviews.json', extract_features=None):
-    # TODO Magic number; better way to handle this in future?
+    # TODO Magic number; better way to handle this in future? Don't want to read whole file
     length_of_examples = 15714
     accuracies = []
 
     example_stream = preprocess.stream_examples(filename, extract_features)
 
-    # Generate a list of indices for each example and shuffle them
-    indices = range(length_of_examples)
-    shuffle(indices)
-
-    chunk_size = length_of_examples / 5
-
-    training_sets = []
-    testing_sets = []
-
-    # For each of the possible five folds:
-    for i in range(5):
-        # We iterate over each fifth of the indices to be used as the test set.
-        # Build a left and right boundary of this region:
-        left_boundary = i * chunk_size
-        right_boundary = left_boundary + chunk_size
-
-        # Generate a list of the indices into the indices array
-        # e.g. training_indices = [0, 1, 4, 5, 6, 7, 8, 9]
-        #      testing_indices  = [2, 3]
-        #      indices          = [2, 4 ,3, 7, 6, 5 ,8, 1, 9, 0]
-        #      So that indices[2] and indices[3] (namely, 3 and 7) are the indices
-        #      into the example_stream that we'll use for testing
-        training_indices = range(left_boundary) + range(right_boundary, length_of_examples)
-        testing_indices = range(left_boundary, right_boundary)
-
-        # Build a set for the actual indices we'll use for training and testing
-        training_set = {indices[x] for x in training_indices}
-        testing_set = {indices[x] for x in testing_indices}
-
-        # Throw them into the list of sets.  Now:
-        #
-        #     index in training_sets[k]
-        #
-        # Will tell us if the index from the stream is in the training set
-        # of fold k
-        training_sets.append(training_set)
-        testing_sets.append(testing_set)
-
-    testing_examples = [[], [], [], [], []]
-
-    # Should return list of five empty models
-    models = initialize_models()
-
-    # For each example in the stream:
-    for index, example in enumerate(example_stream):
-        # Look at each fold
-        for k in range(5):
-            # And if the example we're looking at is part of training for that
-            # fold, add it stochastically for the model
-            if index in training_sets[k]:
-                models[k] = train_with_example(models[k], example)
-
-            # or add it to our list of test examples if it's for testing.
-            # TODO: This will cause memory issues again, since we'll end
-            # up with the entire dataset in memory again.  Will have to modify
-            # to re-stream it.
-            elif index in testing_sets[k]:
-                testing_examples[k].append(example)
+    models, testing_examples = train_models(example_stream, length_of_examples, initialize_models, train_with_example)
 
     # Now run tests for each fold:
     for k in range(5):
@@ -83,6 +26,67 @@ def engage(initialize_models, train_with_example, model_test, filename='../data/
     overall_accuracy = mean(accuracies)
 
     print('Overall accuracy: ' + str(overall_accuracy))
+
+
+def train_models(example_stream, length_of_examples, initialize_models, train_with_example):
+    models = initialize_models()
+    testing_examples = [[], [], [], [], []]
+    training_sets, testing_sets = generate_k_fold_indices(length_of_examples)
+
+    for index, example in enumerate(example_stream):
+        for k in range(5):
+            if index in training_sets[k]:
+                models[k] = train_with_example(models[k], example)
+            # TODO Handle potential memory issues
+            elif index in testing_sets[k]:
+                testing_examples[k].append(example)
+
+    return models, testing_examples
+
+
+def generate_k_fold_indices(length_of_examples):
+    training_sets = []
+    testing_sets = []
+
+    stream_indices = range(length_of_examples)
+    shuffle(stream_indices)
+
+    chunk_size = length_of_examples / 5
+
+    # For every fold:
+    for i in range(5):
+        # e.g. 1000, 2000 for N = 5000 and k = 1
+        left_boundary, right_boundary = get_boundaries_of_fold(chunk_size, i)
+
+        # e.g. training_indices = [0, 1, ..., 999, 2000, 2001, ..., 4999] for N = 5000 and k = 1
+        testing_indices, training_indices = calculate_fold_indices(length_of_examples, left_boundary, right_boundary)
+
+        # Build a set for the actual indices we'll use for training and testing
+        training_set = {stream_indices[x] for x in training_indices}
+        testing_set = {stream_indices[x] for x in testing_indices}
+
+        # Throw them into the list of sets.  Now:
+        #
+        #     index in training_sets[k]
+        #
+        # Will tell us if the index from the stream is in the training set
+        # of fold k
+        training_sets.append(training_set)
+        testing_sets.append(testing_set)
+
+    return training_sets, testing_sets
+
+
+def calculate_fold_indices(length_of_examples, left_boundary, right_boundary):
+    training_indices = range(left_boundary) + range(right_boundary, length_of_examples)
+    testing_indices = range(left_boundary, right_boundary)
+    return testing_indices, training_indices
+
+
+def get_boundaries_of_fold(chunk_size, i):
+    left_boundary = i * chunk_size
+    right_boundary = left_boundary + chunk_size
+    return left_boundary, right_boundary
 
 
 def evaluate(model, model_test, testing_examples):
