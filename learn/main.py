902a5724155685335c9b6b681eb939f8eff4c28d
Joel Roggeman <joelroggeman@gmail.com>
2016-03-27 15:07:49 -0400

    Stochastic training works

--- learn/main.py
+++ learn/main.py
@@ -1,35 +1,88 @@
 from random import shuffle
 import preprocess
+import pdb
 
-def engage(train_model, model_test, file = '../data/reviews.json', extract_features = None):
+def engage(initialize_models, train_model_stochastic, model_test, file = '../data/reviews.json', extract_features = None):
 
     # TODO Magic number; better way to handle this in future?
     length_of_examples = 15714
     chunk_size = length_of_examples / 5
     accuracies = []
-    for i in range(5):
-        # TODO Waste lots of work at the moment
-        example_stream = preprocess.stream_examples(file, extract_features)
 
-        print('Running fold ' + str(i))
-        # Calculate indices for boundaries of test fold
+    example_stream = preprocess.stream_examples(file, extract_features)
+
+    # Generate a list of indices for each example and shuffle them
+    indices = range(length_of_examples)
+    shuffle(indices)
+
+    chunk_size = length_of_examples / 5
+
+    training_sets = []
+    testing_sets = []
+
+    # For each of the possible five folds:
+    for i in range(5):
+
+        # We iterate over each fifth of the indices to be used as the test set.
+        # Build a left and right boundary of this region:
         left_boundary = i * chunk_size
         right_boundary = left_boundary + chunk_size
 
-        # Calculate the indices of the examples to use
+        # Generate a list of the indices into the indices array
+        # e.g. training_indices = [0, 1, 4, 5, 6, 7, 8, 9]
+        #      testing_indices  = [2, 3]
+        #      indices          = [2, 4 ,3, 7, 6, 5 ,8, 1, 9, 0]
+        #      So that indices[2] and indices[3] (namely, 3 and 7) are the indices
+        #      into the example_stream that we'll use for testing
         training_indices = range(left_boundary) + range(right_boundary, length_of_examples)
         testing_indices = range(left_boundary, right_boundary)
 
-        model, testing_examples = train_model(example_stream, training_indices, testing_indices)
+        # Build a set for the actual indices we'll use for training and testing
+        training_set = {indices[x] for x in training_indices}
+        testing_set = {indices[x] for x in testing_indices}
 
-        number_correct = evaluate(model, model_test, testing_examples)
+        # Throw them into the list of sets.  Now:
+        #
+        #     index in training_sets[k]
+        #
+        # Will tell us if the index from the stream is in the training set
+        # of fold k
+        training_sets.append(training_set)
+        testing_sets.append(testing_set)
 
-        print(str(i) + '. Results:')
+    testing_examples = [[], [], [], [], []]
 
-        print(str(number_correct) + '/' + str(len(testing_examples)) + ' = ' +
-                str(number_correct / float(len(testing_examples))) + '%')
+    # Should return list of five empty models
+    models = initialize_models()
 
-        accuracies.append(number_correct / float(len(testing_examples)))
+    # For each example in the stream:
+    for index, example in enumerate(example_stream):
+        # Look at each fold
+        for k in range(5):
+            # And if the example we're looking at is part of training for that
+            # fold, add it stochastically for the model
+            if index in training_sets[k]:
+                models[k] = train_model_stochastic(models[k], example)
+
+            # or add it to our list of test examples if it's for testing.
+            # TODO: This will cause memory issues again, since we'll end
+            # up with the entire dataset in memory again.  Will have to modify
+            # to re-stream it.
+            elif index in testing_sets[k]:
+                testing_examples[k].append(example)
+
+    pdb.set_trace()
+
+    # Now run tests for each fold:
+    for k in range(5):
+        number_correct = evaluate(models[k], model_test, testing_examples[k])
+
+        print(str(k + 1) + '. Results:')
+
+        print(str(number_correct) + '/' + str(len(testing_examples[k])) + ' = ' +
+                str(number_correct / float(len(testing_examples[k]))) + '%')
+
+        accuracies.append(number_correct / float(len(testing_examples[k])))
 
     overall_accuracy = mean(accuracies)
 
