f674c98aeaef95ae189ffeb97133eef45fe3df43
Steve <sae48@pitt.edu>
2016-04-10 17:54:25 -0400

    renamed joal to neuralnet

--- learn/nn_model_factory.py
+++ learn/nn_model_factory.py
@@ -4,11 +4,13 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 
 
-def joal(features):
+def neuralnet(features, sample_size=20, hidden_neurons=3):
     """
     This function takes in a list of features (functions that take in an example and return some value based on it)
     and creates and engages a neural network model, returning a dictionary with results from testing.
     :param features: list of functions to act as features in a NN model
+    :param sample_size:
+    :param hidden_neurons:
     :return: result dictionary, keys are which model was being tested (0-5),
             values are tuples of form (number correct, number tested, percent correct)
     """
@@ -16,13 +18,16 @@ def joal(features):
         return [NNModel(features) for _ in range(5)]
 
     def train_model(model, examples):
+        print 'adding examples'
         for example in examples:
             model.dataset.addSample(tuple([func(example) for func in model.features]), (example.votes['useful'] > 0,))
-
-        model.network = buildNetwork(len(model.features), 3, 1)  # 3 hidden layers for now. can change at any time
+        print 'building network'
+        model.network = buildNetwork(len(model.features), hidden_neurons, 1)
         model.trainer = BackpropTrainer(model.network, model.dataset)
+        print 'training network'
         model.trainer.train()
         model.trainer.trainUntilConvergence()
+        print 'done'
         return model
 
     def model_test(model, example):
@@ -30,7 +35,7 @@ def joal(features):
         return model_result == (example.votes['useful'] > 0)
 
     jole = Jole(initialize_models, train_model, model_test)
-    return engage(jole, filename='../data/smaller_reviews.json', stochastic=False, sample_size=20)
+    return engage(jole, filename='../data/smaller_reviews.json', stochastic=False, sample_size=sample_size)
 
 
 class NNModel(object):
@@ -48,7 +53,7 @@ class Jole(object):
 
 
 if __name__ == '__main__':
-    result = joal([
-        lambda x: 'good' in x.review,
-    ])
+    result = neuralnet([
+        lambda x: len(x.review)
+    ], 30, 3)
     print result
