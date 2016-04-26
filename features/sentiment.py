b134b8b3095849e3114cff4f082886873039574c
shelley <sag109@pitt.edu>
2016-04-26 15:49:39 -0400

    added strong sentiment feature

--- features/sentiment.py
+++ features/sentiment.py
@@ -8,4 +8,17 @@ def sentiment_variance(review):
     polarities = utils.sentiment(review.review)
     return numpy.var([x['compound'] for x in polarities],dtype=numpy.float64)
 
+def raw_sentiment(review):
+    return sum(x['compound'] for x in utils.sentiment(review.review))
+
+def strong_sentiment(review):
+    count = 0
+    for x in utils.sentiment(review.review):
+        if x['neg'] > 0.5:
+            count -=1
+        elif x['pos'] > 0.5:
+            count +=1
+
+    return count
+
 
