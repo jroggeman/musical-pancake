80eb41905a50edeb014e046b0a9254d65aa38457
Stephen Ellis <saellis@Stephens-MacBook-Pro.local>
2016-04-26 17:07:04 -0400

    combinations of feature functions

--- learn/slee.py
+++ learn/slee.py
@@ -2,6 +2,7 @@ from svm_factory import supportvector
 from features import sentiment
 from features.feature_group import Group
 from nn_model_factory import neuralnet
+import itertools
 from bow import bow
 
 
@@ -15,36 +16,47 @@ def slee(seq, group_features, features, model, n):
         - Name Credit: Joel Roggeman
     Recommend running overnight.
     """
-    g = Group(group_features)
-    all_features = [g.call_all_features] + features
-    models = {
-        'nn': ('nn', lambda features, num: neuralnet(features, num, len(g.feature_list) + 5)),
-        'svm': ('svm', lambda features, num: supportvector(features, num))
-    }
-    if model in models:
-        functions = [models[model]]
-    else:
-        functions = [models['nn'], models['svm']]
 
     total_results = []
-    for name, model in functions:
-        print ' {} '.format(name).center(30, '#')
-        for num in seq:
-            print ' Size = {} '.format(num).center(30, '-')
-            result = model(all_features, num)
-            avg_accuracy = mean([result[key][2] for key in result])
-            total_results.append((avg_accuracy, num, name, all_features))
+    for gf_i, _ in enumerate(group_features):
+        for gfs in itertools.combinations(group_features, gf_i + 1):
+            g = Group(list(gfs))
+            for f_i, _ in enumerate(features):
+                for fs in itertools.combinations(features, f_i + 1):
+                    all_features = [g.call_all_features] + list(fs)
+
+                    models = {
+                        'nn': ('nn', lambda features, num: neuralnet(features, num, len(g.feature_list) + 5)),
+                        'svm': ('svm', lambda features, num: supportvector(features, num))
+                    }
+                    if model in models:
+                        functions = [models[model]]
+                    else:
+                        functions = [models['nn'], models['svm']]
+
+                    for name, model in functions:
+                        print ' {} '.format(name).center(30, '#')
+                        for num in seq:
+                            print ' Size = {} '.format(num).center(30, '-')
+                            result = model(all_features, num)
+                            avg_accuracy = mean([result[key][2] for key in result])
+                            total_results.append((avg_accuracy, num, name, list(gfs) + all_features[1:]))
+
+
     sorted_results = sorted(total_results, key=lambda x: x[0], reverse=True)  # sorts on accuracy high-low
     print ' RESULTS '.center(30, '*')
     for i, res in enumerate(sorted_results[:n]):
-        print '{}: accuracy: {}, model: {}, functions: {}'.format(i + 1, *res)
+        print '{}: accuracy: {}, number of samples: {}, model: {}, functions: {}'.format(i + 1, *res)
     print ''.center(30, '*')
 
 
 def main():
-    """Runs slee(). Change these 5 parameters as you see fit to test whatever you'd like."""
-    # 1) sequence: training example counts for each run
-    seq = [10, 20]
+    """Change these 5 parameters as you see fit to test whatever you'd like.
+       It will iterate through permutations of features and models (if you pick 'both') and returns
+       the top n parameter settings
+    """
+    # 1) sequence: training example counts for each run. I'd recommend keeping this kinda low to see which parameter settings do best, then test higher numbers on those parameters
+    seq = [100]
 
     # 2) group_features: the functions that need examples passed in one at a time
     group_features = [sentiment.sentiment_variance, sentiment.raw_sentiment]
@@ -58,7 +70,7 @@ def main():
     model = 'both'
 
     # 5) n: the number of top results to display after executing. if n > the number of results, it prints all of them
-    n = 2
+    n = 5
 
     slee(seq, group_features, features, model, n)
 
