f7e239f72568a4f4cfcf0304030413425d64e866
Joel Roggeman <joelroggeman@gmail.com>
2016-04-26 15:56:48 -0400

    Added smog

--- learn/scores.py
+++ learn/scores.py
@@ -2,6 +2,12 @@ from textstat.textstat import textstat
 
 
 def readability(example):
-    if example is None:
+    if not example:
         return 100
     return textstat.flesch_reading_ease(example.review)
+
+def smog(example):
+    if not example:
+        return 0
+
+    return textstat.smog_index(example.review)
