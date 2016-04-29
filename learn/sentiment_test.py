5c56a54aa816c49feb8c7e7eb385f61a5c838dae
shelley <sag109@pitt.edu>
2016-04-28 23:59:25 -0400

    results for some tests

--- learn/sentiment_test.py
+++ learn/sentiment_test.py
@@ -5,16 +5,110 @@ from nn_model_factory import neuralnet
 from bow import bow
 from scores import readability, smog
 from features import pos
+import traceback
 
+num = 5000
 
-seq = [15]
+g = Group([sentiment.sentiment_variance])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
 
-g = Group([sentiment.sentiment_variance, sentiment.raw_sentiment, sentiment.strong_sentiment, pos.adj_count, pos.adj_entropy, readability, smog])
-
-
-
-print "###############################all svm#####################################"
-for num in seq:
-    print "----------------------------------Size = %s---------------------------------------" %num
+g = Group([sentiment.sentiment_variance, sentiment.raw_sentiment, sentiment.strong_sentiment, smog])
+print "svm".center(30,"#")
+print str(g.feature_list)+"bow"
+try:
     supportvector([g.call_all_features,bow],num)
+except:
+    traceback.print_exc()
 
+
+
+print "svm".center(30,"#")
+print "bow"
+try:
+    supportvector([bow],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([smog])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.sentiment_variance])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.raw_sentiment])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.sentiment_variance, sentiment.strong_sentiment])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.sentiment_variance, sentiment.strong_sentiment, smog])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.sentiment_variance, sentiment.raw_sentiment, sentiment.strong_sentiment])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+g = Group([sentiment.sentiment_variance,smog])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+g = Group([sentiment.raw_sentiment, smog])
+print "svm".center(30,"#")
+print str(g.feature_list)
+try:
+    supportvector([g.call_all_features],num)
+except:
+    traceback.print_exc()
+
+
+g = Group([sentiment.raw_sentiment, smog])
+print "svm".center(30,"#")
+print str(g.feature_list)+"bow"
+try:
+    supportvector([g.call_all_features,bow],num)
+except:
+    traceback.print_exc()
